4407f2ed8cec0c85745d9d2005983ce81dded585
Lorenarojas1 <lorena.rojas080912@gmail.com>
2022-12-20 23:19:56 -0600

    Feat: create first in Java

---
+++ HolaMundoJava/src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declare a variable
+        int speed;
+
+        // Give a value to a variable
+        speed = 10;
+        System.out.println(speed);
+
+        // Declare and give value to a variable
+        int salary = 1000;
+
+        // Create a string variable
+        String employeeName = "Lorena Rojas";
+        System.out.println(employeeName);
+    }
+}

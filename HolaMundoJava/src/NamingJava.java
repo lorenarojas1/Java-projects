e79477a1ff9d041f610ee210834f34961d728e1e
Lorenarojas1 <lorena.rojas080912@gmail.com>
2022-12-21 12:47:00 -0600

    Feat: Convencional naming variables

---
+++ HolaMundoJava/src/NamingJava.java
@@ -0,0 +1,18 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int cellphone = 554766088;
+        int cellPhone = 554766047;
+        System.out.println(cellphone);
+        System.out.println(cellPhone);
+
+        String $countryName = "Spain";
+        String _backgroundColor = "Green";
+
+        String currency$ = "MXN";
+        String background_color = "BLUE";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH =1;
+    }
+}

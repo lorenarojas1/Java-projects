76c80005e1785f05a51b86bf80056aeb7eda4d71
Lorenarojas1 <lorena.rojas080912@gmail.com>
2023-01-03 17:30:30 -0600

    Feat: examples using casting

--- HolaMundoJava/src/Casting.java
+++ HolaMundoJava/src/Casting.java
@@ -32,5 +32,45 @@ public class Casting {
         // specific cast of char to short
         short nS = (short) n;
         System.out.println(nS); // 49
+
+        // Examples
+        char letter = 'z';
+        int letterToInt = letter;
+        System.out.println("char to int: " + letterToInt); // 122 - alt + 122 = z
+
+        int i = 250;
+        long iL = i;
+        short iS = (short) iL;
+        System.out.println("int to long: " + iL); // 250
+        System.out.println("long to short: " + iS); // 250
+
+        double amount = 301.067;
+        long amountL = (long) amount;
+        System.out.println("double to long: " + amountL); // 301
+
+        int calculate = 100;
+        calculate += 5000.66;
+        float calculateF = (float) calculate;
+        System.out.println("int to int: " + calculate); // 5100
+        System.out.println("int to float: " + calculateF); // 5100.0
+
+        int calculateC = 100;
+        double calculateD = calculateC + 5000.66;
+        float calculateE = (float) calculateD;
+        System.out.println("int to double: " + calculateD); // 5100.66
+        System.out.println("int to float: " + calculateF); // 5100.0
+
+
+        int product = 737;
+        product *= 100;
+        byte productB = (byte) product;
+        System.out.println("int to int: " + product); // 73700
+        System.out.println("int to byte: " + productB); // -28
+
+        double partition = 298.638;
+        partition /= 25;
+        long partitionL = (long) partition;
+        System.out.println("double to double: " + partition); // 11.9455...
+        System.out.println("double to long: " + partitionL); // 11
     }
 }

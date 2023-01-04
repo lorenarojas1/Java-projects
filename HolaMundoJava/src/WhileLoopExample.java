99e7981d48697a5a4f5442da838a8d11278643bb
Lorenarojas1 <lorena.rojas080912@gmail.com>
2023-01-04 01:13:18 -0600

    Feat: Create Arrays and Loops to print arrays

---
+++ HolaMundoJava/src/WhileLoopExample.java
@@ -0,0 +1,16 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int x= 1;
+        while (x <= 10){
+            System.out.println(++x);
+        }
+
+        int i = 7;
+        char c = 'w';
+        System.out.println((i >= 6) && (c == 'w'));
+
+        int a=1, j=2, k=3, m=2;
+        System.out.println ((j >= a) || (k == m));
+    }
+
+}

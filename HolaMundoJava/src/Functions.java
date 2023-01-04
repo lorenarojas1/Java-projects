9a656edc5de97e1a351ce827d949854b96077ce9
Lorenarojas1 <lorena.rojas080912@gmail.com>
2023-01-03 20:35:46 -0600

    Feat: Create functions

---
+++ HolaMundoJava/src/Functions.java
@@ -0,0 +1,34 @@
+public class Functions {
+    public static void main(String[] args) {
+        double y = 3;
+
+        circleArea(y);
+        sphereArea(y);
+        sphereVolume(y);
+
+        System.out.println("MXN to Dollars: " + convertToDollar(200, "MXN"));
+        System.out.println("COP to Dollars: " + convertToDollar(200, "COP"));
+        
+    }
+    public static double circleArea(double r){
+        return Math.PI * Math.pow(r,2);
+    }
+    public static double sphereArea(double r){
+        return 4 * Math.PI * Math.pow(r,2);
+    }
+    public static double sphereVolume(double r){
+        return (4/3) * Math.PI * Math.pow(r, 3);
+    }
+    public static double convertToDollar(double quantity, String currency){
+        // MXN COP
+        switch (currency){
+            case "MXN":
+                quantity =quantity *0.052;
+                break;
+            case "COP":
+                quantity =quantity *0.00031;
+                break;
+        }
+        return quantity;
+    }
+}
